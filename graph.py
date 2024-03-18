26aee7095024f24a988219284dbcca5190aca1f7
Sreeram Vasudevan <tpasreeram@gmail.com>
2024-03-17 20:11:37 -0400

    Finished basic functionality for graph.py

--- graph.py
+++ graph.py
@@ -5,21 +5,12 @@ import numpy as np
 
 def main():
     filename = sys.argv[1]
-    print(filename)
     
     df = pd.read_csv(filename, sep=',')
 
-    fig, ax1 = plt.subplots()
-
-    ax1.plot(df['offset'], df['DC_X'], 'b')
-    ax1.axis((7500,30000, -20, 220))
-    ax2 = ax1.twinx()
-    ax2.plot(df['offset'], df['Live_ME_(experimental)'], 'r')
-    ax2.axis((7500,30000,-2.5e6,-1.0e6))
-    plt.show()
-    
-    th = 0
-    tl = 1
+    tl = 106        # Temp 1
+    th = 107.3      # Temp 2 (Temp2 > Temp1 need not be true)
+    tol = 0.001     # Tolerance for setpoint temps
 
     if tl == th:
         th += 0.1
@@ -28,21 +19,44 @@ def main():
         th = tl
         tl = tempth
 
-
     # x-axis : offset, temperature
     # y-axis_1 : DC_X
     # y-axis_2 : Live_ME_..., f_dot/f
-    xaxis = "offset", "temperature"
+    xaxis = "offset"
     yaxis1 = "DC_X"
-    yaxis2 = "Live_ME_(experimental)", "f"
-    extraaxis = "fdot"
+    yaxis2 = "Live_ME_(experimental)"
+    
+    # Low End
+    templ = df.loc[df["Setpoint"].between(tl - tol, tl + tol)]
+    firstl = df.loc[templ["offset"].idxmin()]
 
-    closest_tl = df.loc[(df["Setpoint"] - tl).abs().idxmin()]
-    x_low = closest_tl[xaxis]
+    # High End
+    temph = df.loc[df["Setpoint"].between(th - tol, th + tol)]
+    firsth = df.loc[temph["offset"].idxmin()]
 
-    closest_th = df.loc[(df["Setpoint"] - th).abs().idxmin()]
-    x_high = closest_th[xaxis]
+    # firstl.name, firsth.name
+    graphr = df.loc[firstl.name:firsth.name]
 
+    xmin = graphr[xaxis].min()
+    xmax = graphr[xaxis].max()
+
+    y1r = (graphr[yaxis1].max() - graphr[yaxis1].min()) * 0.2
+    y1min = np.floor(graphr[yaxis1].min() - y1r)
+    y1max = np.ceil(graphr[yaxis1].max() + y1r)
+
+    y2r = (graphr[yaxis2].max() - graphr[yaxis2].min()) * 0.2
+    y2min = np.floor(graphr[yaxis2].min() - y2r)
+    y2max = np.ceil(graphr[yaxis2].max() + y2r)
+
+    fig, ax1 = plt.subplots()
+
+    ax1.plot(df['offset'], df['DC_X'], 'b')
+    ax1.axis((xmin,xmax, y1min, y1max))
+    ax2 = ax1.twinx()
+    ax2.plot(df['offset'], df['Live_ME_(experimental)'], 'r')
+    ax2.axis((xmin,xmax,y2min,y2max))
+    plt.show()
+    
     return 0
 
 main()
